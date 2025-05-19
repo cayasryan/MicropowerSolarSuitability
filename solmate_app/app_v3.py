bae9c78d58421801e64d0f7a730837c86d7a4ccc
cayasryan <rrcayas@gmail.com>
2025-05-19 13:41:14 +0800

    change favicon

--- solmate_app/app_v3.py
+++ solmate_app/app_v3.py
@@ -14,7 +14,7 @@ from solmate_app.ui.results_page import show_results_page
 
 st.set_page_config(
     page_title="SolMate – Solar Site Assessment Tool",
-    page_icon="https://emojiterra.com/sun-with-face/",
+    page_icon="🌞",
     layout="wide",
 )
 
